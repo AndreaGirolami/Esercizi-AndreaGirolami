afb05d1b2896813a3da933c2fea7219f37dc594a
AndreaGirolami <A.Girolami@almaviva.it>
2023-04-05 09:59:28 +0200

    routing

--- esercizi/src/app/app.module.ts
+++ esercizi/src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MaterialModule } from './material/material.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { OtherComponent } from './other/other.component';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ServiceComponent,
+    OtherComponent,
+    AboutComponent,
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     MatToolbarModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
